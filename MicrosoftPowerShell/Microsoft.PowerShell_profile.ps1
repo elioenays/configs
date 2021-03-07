66be90fe933bd1d9c9b4a5f308acab57007cb62a
Elioenay Silva <elionay407@gmail.com>
2021-03-07 00:27:40 -0300

    Updates on files configs

---
+++ MicrosoftPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme Star
+
+Set-Variable www '~\www\'
+
+Import-Module PSReadLine
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
+
+#Set the color for Prediction (auto-suggestion)
+# Set-PSReadlineOption -Colors @{Prediction = '#686868' }
